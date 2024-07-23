86276e0d5de12b1a404ae249b01df92f182cf1df
Kyle Gan <kyle_elijah_gan@dlsu.edu.ph>
2024-07-24 04:01:44 +0800

    Added driver file

---
+++ src/Driver.java
@@ -0,0 +1,28 @@
+public class Driver {
+    public static void main (String[] args){
+        HotelReservationSystem HRS = new HotelReservationSystem();
+        ViewHRS view = new ViewHRS();
+        ControlHRS controller = new ControlHRS(HRS, view);
+        boolean exit = false;
+        while(!exit){
+            controller.printMenu();
+            int option = InputHelper.nextInt();
+            switch (option){
+                case 1:
+                    controller.createHotel();
+                    break;
+                case 2:
+                    controller.viewHotel();
+                    break;
+                case 3:
+                    controller.manageHotel();
+                    break;
+                case 4:
+                    controller.bookReservation();
+                    break;
+                case 5:
+                    exit = true;
+            }
+        }
+    }
+}
